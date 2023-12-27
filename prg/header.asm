307a5d1a1c78505083927a0ae55a95193a3eb79c
bunder2015 <omfgbunder@gmail.com>
2023-12-26 19:46:48 -0500

    fix log2 math in internal rom header
    i suck at math :)
    
    Signed-off-by: bunder2015 <omfgbunder@gmail.com>

--- prg/header.asm
+++ prg/header.asm
@@ -8,8 +8,8 @@
 	.asciiz "BOILERPLATE-SNES    " 	; 21 characters
 	.byte %00110000 		; FastROM
 	.byte %00000010			; ROM / SRAM / Battery
-	.byte $14			; ROM size (32k x 128 banks, log2)
-	.byte $05			; RAM size (32k x 1 bank, log2)
+	.byte $16			; ROM size (32k x 128 banks, log2)
+	.byte $0f			; RAM size (32k x 1 bank, log2)
 	.byte $01			; Region (North America)
 	.byte $ff			; Developer ID
 	.byte $01			; Version number
